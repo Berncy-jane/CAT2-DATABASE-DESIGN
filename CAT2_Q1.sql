7481de608aaf12b0a909e4444885e36745331981
Berncy-jane <berncyjane960@gmail.com>
2024-03-19 19:11:00 +0300

    question 1

---
+++ CAT2_Q1.sql
@@ -0,0 +1,12 @@
+CREATE TABLE department(
+    dept_ID INT AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(20),
+    building VARCHAR(20),
+    budget NUMERIC(12, 2)
+);
+INSERT INTO department(dept_name, building, budget) VALUES
+('IT','Building A', 50000000)
+('Marketing','Building B', 1000000)
+('Finance','Building C', 2000000)
+('Operations','Building D', 500000)
+('Human Resources','Building E', 100000)
